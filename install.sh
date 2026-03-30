ee8f440032d2fa2b8fbd9a002aa88d06266643d6
jalil <alawih352@gmail.com>
2026-03-30 22:27:17 +0100

    filet

--- install.sh
+++ install.sh
@@ -1501,7 +1501,7 @@ fi
 
 DARK_BASE64=$(echo -n "$DARK_JSON" | base64 -w 0)
 DARK_LINK="darktunnel://$DARK_BASE64"
-DARK_FILE="${LINK_FRAGMENT}.dark"
+DARK_FILE="${LINK_FRAGMENT}.txt"
 
 echo "$DARK_LINK" > "$DARK_FILE"
 # -------- Generate Alternative URL (short URL) --------
